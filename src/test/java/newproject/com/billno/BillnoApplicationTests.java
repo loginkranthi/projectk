4c0b6f61889c03245643c60d4e0afe1eb361f2d8
loginkranthi <loginkranthi@gmail.com>
2022-11-15 21:15:42 +0530

    new

---
+++ src/test/java/newproject/com/billno/BillnoApplicationTests.java
@@ -0,0 +1,13 @@
+package newproject.com.billno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
